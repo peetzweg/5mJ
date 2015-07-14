0726a76151e8d49737b1538f43c0c88cd86f5123
Gunter <mnml@mnml-mbp.fritz.box>
2015-07-14 22:35:42 +0200

    changing background color depending on the current time

--- js/init.js
+++ js/init.js
@@ -1,8 +1,54 @@
-(function($){
-  $(function(){
-    var url = 'http://tinybuddha.com/wp-content/plugins/tiny-buddha-host/wisdom.txt'
-    $.get(url, function(data) {
-       alert(data);
-    });
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+
+
+var dayBGColor = "#fff"
+var dayColor = "black"
+
+var nightBGColor = "#222"
+var nightColor = "white"
+
+var nighttime = false
+
+var dayQuestions = ["I am grateful for", "What would make today great?", "Daily affirmations. I am..."]
+var nightQuestions = ["3 Amazing things that happened today...", "How could I have made today better?"]
+
+
+function setColors(){
+  var bgColor
+  var textColor
+  if(nighttime){
+    bgColor = nightBGColor
+    textColor = nightColor
+  } else {
+    bgColor = dayBGColor
+    textColor = dayColor
+  }
+
+  $('body').css( "background-color", bgColor );
+  $('body').css( "color", textColor );
+}
+
+function setHeading(){
+  $('#heading').text(moment().format('dddd, MMMM Do, YYYY'));
+
+}
+
+function addInputs(){
+}
+
+function init( jQuery ) {
+  if(moment().hour() >= 18){
+    nighttime = true
+  }
+
+  setColors()
+  setHeading()
+  addInputs()
+
+
+  $(':input').bind('input propertychange', function(){
+    console.log($(this).attr("id") + " = " + $(this).val());
+  });
+}
+
+
+$( document ).ready( init );
